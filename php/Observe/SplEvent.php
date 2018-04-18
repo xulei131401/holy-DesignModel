0f0bc4f75c32ee366abf195e2ce10944bb003043
许磊 <373045134@qq.com>
2018-04-18 16:52:08 +0800

    提交观察者模式

---
+++ php/Observe/SplEvent.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xulei
+ * Date: 2018/4/18
+ * Time: 15:44
+ */
+
+class SplEvent
+{
+    public $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+}
